d4b46a1fbc35a0ff73b5e39183802cd80f5eddb7
Bastian Kerk <smark@cosfans.de>
2018-11-21 01:08:58 +0100

    Welcome Page Created
    Thank you for watching the stream. We set up a discord server for the feature request list

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cosbook
+The open source social media network for people with a cosplay conection.
+
+##Discord
+If you want to have some features in our social network then just join our discord server on request them:
+https://discord.gg/j23zt5
+
+##Contributor Ideas
+###Possible Network Names
+edencraftdesign: Cosbook<br/>
+Andre88uk: Wardrobe
+###Possible Network Slogans
+Andre88uk: "Why just play when you can cosplay ?"
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,165 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<title>Hello Streamers out there</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+</head>
+<body class="hold-transition login-page">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+<div class="col-xs-12 network_name_wrapper" style="margin-bottom: 200px;">
+    <div class="col-xs-12">
+        <div class="widget-user-header bg-green" style="padding: 1px 25px 20px 45px;">
+            <h1 style="margin-left: 0px;" class="widget-user-username">Wardrobe</h1>
+            <h2 style="margin-left: 0px;" class="widget-user-desc">Why just play when you can cosplay?</h2>
+        </div>
+    </div>
+</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<div class="col-xs-12">
+    <div class="col-xs-12 col-lg-4">
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            <form action="../../index2.html" method="post">
+                <div class="form-group has-feedback">
+                    <input type="email" class="form-control" placeholder="Email">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" placeholder="Password">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="form-group">
+                    <select class="form-control">
+                        <option value="" selected disabled>How did you hear about us</option>
+                        <option>Discord Channel</option>
+                        <option>Facebook</option>
+                        <option>From a Friend</option>
+                        <option>Instagram</option>
+                        <option>Twitch</option>
+                    </select>
                 </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox"> Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center">
+                <p>- OR -</p>
+                <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fab fa-facebook"></i> Sign in using Facebook</a>
+                <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fab fa-google-plus"></i> Sign in using Google+</a>
+            </div>
+            <a href="#">I forgot my password</a><br>
+        </div>
+    </div>
+    <div class="col-xs-12 col-lg-4">
+        <div class="box box-widget widget-user-2" style="min-height: 414px;">
+            <div class="widget-user-header bg-yellow">
+                <div class="widget-user-image">
+                    <img class="img-circle" src="/dist/img/user7-128x128.jpg" alt="User Avatar">
+                </div>
+                <h3 class="widget-user-username">Bastian Kerk</h3>
+                <h5 class="widget-user-desc">Lead Developer</h5>
+            </div>
+            <div class="box-footer">
+                <p style="padding: 0px 5px 0px 5px;">This Social Network is totally a open source development project. The project will be a social network for people who are any way connected with the cosplay scene.</p>
+                <p style="padding: 0px 5px 0px 5px;">You could follow the process of the development like you want to.</p>
+
+            </div>
+            <div class="box-footer no-padding">
+
+                <ul class="nav nav-stacked">
+                    <li><a title="live develop sessions on twitch" target="_blank" href="https://www.twitch.tv/bastiankerk">Live Development on Twitch <i class="pull-right fa-2x fab fa-twitch"></i></a></li>
+                    <li><a title="Feature Request via Discord Community" target="_blank" href="https://discord.gg/j23zt5">Request Features via Discord <i class="pull-right fa-2x fab fa-discord"></i></a></li>
+                    <li><a title="Get in contact with out open source code" target="_blank" href="https://github.com/bastiankerk">Contribute via Github <i class="pull-right fa-2x fab fa-github"></i></a></li>
+                    <li><a title="Meet the Father of the network on instagram" target="_blank" href="https://www.instagram.com/bastiankerk/">Follow Bastian on Instagram <i class="pull-right fa-2x fab fa-instagram"></i></a></li>
+                </ul>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+    <div class="col-xs-12 col-lg-4">
+        <div class="register-box-body">
+            <p class="login-box-msg">Register a new membership</p>
+
+            <form action="../../index.html" method="post">
+                <div class="form-group has-feedback">
+                    <input type="email" class="form-control" placeholder="Email">
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" placeholder="Password">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" placeholder="Retype password">
+                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox"> I agree to the <a href="#">terms</a>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                    </div>
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center">
+                <p>- OR -</p>
+                <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa-xs fab fa-facebook"></i> Sign up using Facebook</a>
+                <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa-xs fab fa-google-plus"></i> Sign up using Google+</a>
+            </div>
+
+            <a href="login.html" class="text-center">I already have a membership</a>
+        </div>
+    </div>
+</div>
+
+<div style="display: none">
+@if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
+</div>
+    <style>
+        .box-widget .nav .fa-twitch {
+            color: #6441a5;
+        }
+        .box-widget .nav .fa-discord {
+            color: #7289da;
+        }
+        .box-widget .nav .fa-github {
+            color: #000000;
+        }
+        .box-widget .nav .fa-instagram {
+            color: #bc2a8d;
+        }
+        .login-page {
+            background: url("/img/background.jpg") no-repeat;
+            background-size: contain;
+        }
+    </style>
+
+@endsection
